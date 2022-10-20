d38407955b1657e27f0b19f7dfc609244acaed21
RoxanneRlld <roxanne.roulland@ynov.com>
2022-10-20 11:23:07 +0200

    add port range

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
-from scapy.all import IP, sr1, ICMP, ARP, Ether, srp, getmacbyip
+from scapy.all import IP, sr1, ICMP, ARP, Ether, srp, getmacbyip, TCP, sr
+import random
 
 for ping in range(3, 7):
     print("__________________________")
@@ -12,5 +13,42 @@ for ping in range(3, 7):
     mac = getmacbyip(address)
     print(str(mac))
     res = sr1(IP(dst=address, src='192.168.121.4')/ICMP(), timeout=5)
+    if res: 
+        print("Host is up")
+        port_range = [22, 23, 80, 443, 3389]
+
+# Send SYN with random Src Port for each Dst port
+        for dst_port in port_range:
+            src_port = random.randint(1025,65534)
+            resp = sr1(
+            IP(dst=address)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,
+            verbose=0,
+        )
+
+        if resp is None:
+            print(f"{address}:{dst_port} is filtered (silently dropped).")
+
+        elif(resp.haslayer(TCP)):
+            if(resp.getlayer(TCP).flags == 0x12):
+                # Send a gratuitous RST to close the connection
+                send_rst = sr(
+                    IP(dst=address)/TCP(sport=src_port,dport=dst_port,flags='R'),
+                    timeout=1,
+                    verbose=0,
+                )
+                print(f"{address}:{dst_port} is open.")
+
+            elif (resp.getlayer(TCP).flags == 0x14):
+                print(f"{address}:{dst_port} is closed.")
+
+        elif(resp.haslayer(ICMP)):
+            if(
+                int(resp.getlayer(ICMP).type) == 3 and
+                int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]
+            ):
+                print(f"{address}:{dst_port} is filtered (silently dropped).")
 
 
+
+
+# if address == address src ne pas faire l'itération
