2a1a1542ec2f8b32f490f28691fb243c2013b9f4
RoxanneRlld <roxanne.roulland@ynov.com>
2022-10-20 09:38:39 +0200

    add mac

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
-from scapy.all import IP, sr1, ICMP, ARP, Ether, srp
+from scapy.all import IP, sr1, ICMP, ARP, Ether, srp, getmacbyip
 
 for ping in range(3, 5):
     print("__________________________")
     address = "192.168.121." + str(ping)
     print(str(address))
+    mac = getmacbyip(address)
+    print(str(mac))
     arp_request = ARP(pdst=address)
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
@@ -11,3 +13,4 @@ for ping in range(3, 5):
     print(str(answered_list))
     res = sr1(IP(dst=address, src='192.168.121.6')/ICMP(), timeout=5)
 
+# changer les ip, et le for
