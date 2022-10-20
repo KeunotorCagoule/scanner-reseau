853c6224d7de6ea21b8d96d710e64fc3864ec304
RoxanneRlld <roxanne.roulland@ynov.com>
2022-10-20 09:42:10 +0200

    mac at good place

--- script.py
+++ script.py
@@ -4,13 +4,13 @@ for ping in range(3, 5):
     print("__________________________")
     address = "192.168.121." + str(ping)
     print(str(address))
-    mac = getmacbyip(address)
-    print(str(mac))
     arp_request = ARP(pdst=address)
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     print(str(answered_list))
+    mac = getmacbyip(address)
+    print(str(mac))
     res = sr1(IP(dst=address, src='192.168.121.6')/ICMP(), timeout=5)
 
 # changer les ip, et le for
