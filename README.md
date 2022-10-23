15f2842ef10273ed4e8ebf42124fa0d3f52e7121
RoxanneRlld <roxanne.roulland@ynov.com>
2022-10-23 20:54:07 +0200

    langage and add little functions

--- README.md
+++ README.md
@@ -5,3 +5,36 @@ Pour couper le script en python, il faut utiliser Ctrl+Z
 Installer tqdm lib
 
 change le nom du fichier, change w en a
+
+DEV LA PARTIE QUI PERMET D'ITERER SUR TOUUUUTES LES IP D'UN RESEAU
+MAYBE FAIRE DES INPUT POUR RENTRER L'ADRESSE SOURCE, LE RESEAU ET SON MASQUE
+
+TEST
+test address not ping herself
+test test linux os
+
+# Description du script
+Le Scanner Réseau est un script codé en Python.
+A chaque éxecution du script, le script fournira un rapport qui sera nommé avec la date et l'heure du lancement du programme.
+Le script ne passera pas sur l'adresse source qui envoie les pings car ils passeront sur une couche au dessus.
+Le Scanner réseau va permettre d'itérer sur toutes les adresses IP du réseau choisi. Il enverra un ping sur toutes les adresses IP pour voir si elles répondent. Si une adresse IP répond le script inscrira dans le rapport si la machine est allumée ou non et ensuite affichera son adresse MAC.
+Ensuite le script regardera sur des ports principaux prédéfinis à l'avance. Il sera inscrit dans le rapport si les ports sont ouverts, fermés ou filtré.
+A partir des réponses des ports. Le script donnera dans le rapport l'OS de l'adresse IP.
+
+# Initialisation du script
+L'éxecution du script se fait en administrateur.
+Il faudra ouvrir le fichier pour changer l'adresse source ainsi que la partie fixe de l'adresse du réseau ainsi que les ports sur lesquels la machine regardera.
+
+
+## Lancer le script
+Sous Windows, il faudra lancer un terminal en tant qu'administrateur et éxécuter la commande suivante : 
+```bash
+py script.py
+```
+
+Sous Linux, il faudra lancer un terminal classique et éxécuter la commande suivante : 
+```bash
+sudo python3 script.py
+```
+
+
--- script.py
+++ script.py
@@ -3,9 +3,10 @@ import random
 import datetime
 
 date_time = datetime.datetime.now()
-file_name = "Rapport du {}".format(date_time)
+file_name = "Report of {}".format(date_time)
 print(file_name)
 file = open("data.txt", "w")
+#src_address = "192.168.121.4"
 
 
 def decode_binary_string(s):
@@ -13,59 +14,71 @@ def decode_binary_string(s):
 
 
 for ping in range(3, 7):
+    start_address = input("Please enter the address without the bytes you want to iterate over : ")
+    src_address = input("Please enter the source address (the address must be in the specified network) : ")
     file.write("\n__________________________\n")
-    address = "192.168.121." + str(ping)
-    file.write("Adresse IP : " + str(address) + "\n")
-    arp_request = ARP(pdst=address)
-    #file.write(str(arp_request)+ "\n")
-    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    file.write(str(answered_list) + "\n")
-    mac = getmacbyip(address)
-    file.write("Adresse MAC : " + str(mac))
-    res = sr1(IP(dst=address, src='192.168.121.4')/ICMP(), timeout=5)
-    resFr = decode_binary_string(res)
-    print(resFr)
-    file.write("\n" + str(resFr))
-    if res:
-        file.write("\nHost is up")
-        port_range = [22, 23, 80, 443, 3389]
-        file.write("\nEtats des ports principaux :\n")
+    address =  + str(ping)
+    if address == src_address:
+        file.write("Adresse IP : " + str(address) + "\n")
+        arp_request = ARP(pdst=address)
+        #file.write(str(arp_request)+ "\n")
+        broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast/arp_request
+        answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+        file.write(str(answered_list) + "\n")
+        mac = getmacbyip(address)
+        file.write("Address MAC : " + str(mac))
+        res = sr1(IP(dst=address, src=src_address)/ICMP(), timeout=5)
+        resFr = decode_binary_string(res)
+        print(resFr)
+        file.write("\n" + str(resFr))
+        if res:
+            file.write("\nHost is up")
+            #port_range = [22, 23, 80, 443, 3389]
+            input_range = input("Please enter a list of ports to test separated by space :")
+            port_range  = input_range.split()
+            file.write("\nMain port states :\n")
 
-        for dst_port in port_range:
-            src_port = random.randint(1025, 65534)
-            resp = sr1(
-                IP(dst=address)/TCP(sport=src_port, dport=dst_port, flags="S"), timeout=1,
-                verbose=0,
-            )
+            for dst_port in port_range:
+                res = ""
+                src_port = random.randint(1025, 65534)
+                resp = sr1(
+                    IP(dst=address)/TCP(sport=src_port, dport=dst_port, flags="S"), timeout=1,
+                    verbose=0,
+                )
 
-            if resp is None:
-                file.write(
-                    f"{address}:{dst_port} is filtered (silently dropped).\n")
-
-            elif(resp.haslayer(TCP)):
-                if(resp.getlayer(TCP).flags == 0x12):
-                    send_rst = sr(
-                        IP(dst=address)/TCP(sport=src_port,
-                                            dport=dst_port, flags='R'),
-                        timeout=1,
-                        verbose=0,
-                    )
-                    file.write(f"{address}:{dst_port} is open.\n")
-
-                elif (resp.getlayer(TCP).flags == 0x14):
-                    file.write(f"{address}:{dst_port} is closed.\n")
-
-            elif(resp.haslayer(ICMP)):
-                if(
-                    int(resp.getlayer(ICMP).type) == 3 and
-                    int(resp.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]
-                ):
+                if resp is None:
                     file.write(
-                        f"\n{address}:{dst_port} is filtered (silently dropped).")
-    else:
-        file.write("\nHost is down")
+                        f"{address}:{dst_port} is filtered (silently dropped).\n")
+                    res = "dropped"
+
+                elif(resp.haslayer(TCP)):
+                    if(resp.getlayer(TCP).flags == 0x12):
+                        send_rst = sr(
+                            IP(dst=address)/TCP(sport=src_port,
+                                                dport=dst_port, flags='R'),
+                            timeout=1,
+                            verbose=0,
+                        )
+                        file.write(f"{address}:{dst_port} is open.\n")
+                        res = "open"
+
+                    elif (resp.getlayer(TCP).flags == 0x14):
+                        file.write(f"{address}:{dst_port} is closed.\n")
+                        res = "closed"
+                        file.write(f"{address}'s OS is Windows")
+
+                elif(resp.haslayer(ICMP)):
+                    if(
+                        int(resp.getlayer(ICMP).type) == 3 and
+                        int(resp.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]
+                    ):
+                        file.write(
+                            f"\n{address}:{dst_port} is filtered (silently dropped).")
+                elif (dst_port == 22) and (res == "dropped"):
+                    file.write(f"{address}'s is Linux.")
+        else:
+            file.write("\nHost is down")
 
 
 # print(file.read())
